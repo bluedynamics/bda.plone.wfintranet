e2727a321e36fe4175134317cbd67e404e774f10
Jens W. Klein <jens@bluedynamics.com>
2012-02-29 11:46:37 +0100

    init

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+shortdesc = "Intranet Workflow by Bluedynamics Alliance"
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(name='bda.plone.wfintranet',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Environment :: Web Environment',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      license='General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bda', 'bda.plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+      ],
+      extras_require={
+          'test': [
+              'interlude',
+              'zope.configuration',
+              'zope.testing',
+          ]
+      },      
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
